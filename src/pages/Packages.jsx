9dc77dd13b70e3496defcd6f02acc07d176a3852
wtfpratyush <pratyushk92.pk@gmail.com>
2026-09-22 16:14:18 +0530

    Update Past Cruises to balanced 3x2 grid layout (3 and 3 in next line)

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -138,7 +138,7 @@ const Packages = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-7">
             {pastCruises.map((c, i) => (
               <div
                 key={i}
